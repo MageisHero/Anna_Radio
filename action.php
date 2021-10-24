0fdaf1cb654722ac8fa63b31e00a749c9903b394
MageisHero <mageis.hero@gmail.com>
2021-10-24 13:55:30 -0700

    Update action.php

--- action.php
+++ action.php
@@ -1,14 +1,1 @@
-<?php
-//action.php
-    switch($_GET['action']){
-        case 'update':
-            version = (int)$_GET['version'];
-            result = version;
-            echo result;
-        break;
-
-        case 'login':
-        // code goes here
-        break;
-    }
-?>
+1.3
